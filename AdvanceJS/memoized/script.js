fd6b2d9b9ec44f6c4939585863ec06c106a1dd4c
hasuq33 <hardworkerarmy1234@gmail.com>
2025-03-25 07:46:50 +0530

    Memoization

--- AdvanceJS/memoized/script.js
+++ AdvanceJS/memoized/script.js
@@ -1,12 +1,40 @@
 const initApp = () =>{
-    const multiplyBy10 =  cacheMultplicationBy10();
+    const result =  memoize(fib);
 
-    console.log(multiplyBy10(5));
-    console.log(multiplyBy10(5));
-    console.log(multiplyBy10(5));
-    console.log(multiplyBy10(5));
-    console.log(multiplyBy10(5));
-    console.log(multiplyBy10(4));
+    console.log(result(20));
+    console.log(result(20));
+    console.log(result(20));
+}
+
+const add3 = (num1, num2, num3) =>{
+    return num1 + num2 + num3;
+}
+
+
+// This is decorator Function 
+const memoize = (fn) =>{
+    const cache = {};
+
+    return (...args) =>{
+        if(args.toString() in cache){
+            console.log(cache);
+            return cache[args.toString()];
+        }
+        const result = fn(...args);
+        cache[args.toString()] = result;
+        return result;
+    }
+}
+
+const addMany = (...args) =>{
+    return args.reduce((acc,num) => {
+        return acc + num;
+    });
+}
+
+const fib = (pos) =>{
+    if(pos < 2) return pos;
+    return fib(pos-1) + fib(pos-2);
 }
 
 const multiplyBy10 = (num) => num * 10;
@@ -19,5 +47,5 @@ const cacheMultplicationBy10 = () =>{
         return cache[num] = num * 10;
     }
 }
-
-document.addEventListener("DOMContentLoaded",initApp);
+initApp();
+// document.addEventListener("DOMContentLoaded",initApp);
